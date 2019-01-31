e37e65c3cf04784a14f97d32c319df45329a3101
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 15:56:14 +0000

    Create userInput.c

---
+++ c/userInput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    char name[100];
+
+   printf("What is your name?\n");
+   scanf("%s", &name);
+
+   printf("Hello, %s. Have a great day.\n", name);
+
+   return 0;
+}
