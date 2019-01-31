e6df7941ea825e0e8c42e2f2e3cce269f1b80085
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 19:13:38 +0000

    Update 01_variables.cs

--- csharp/01_variables.cs
+++ csharp/01_variables.cs
@@ -1,5 +1,3 @@
-//Compile by mcs Hello.cs
-
 using System;
 
 class Hello{
@@ -9,12 +7,10 @@ class Hello{
         int age = 30;
         double height = 6.1;
         char grade = 'A';
-
-
-
+	    
 	Console.WriteLine("There was a man called {0}.", name);
-    Console.WriteLine("He was {0} years old.", age);
-    Console.Write("He was {0} foot tall and ", height);
-    Console.WriteLine("Scored an {0} on the test.", grade);
+    	Console.WriteLine("He was {0} years old.", age);
+    	Console.Write("He was {0} foot tall and ", height);
+    	Console.WriteLine("Scored an {0} on the test.", grade);
     }
 }
