8d854c88675ea95da2b35c75d107ccef58ad9c26
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 16:02:03 +0000

    Create 01_variables.go

---
+++ go/01_variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Boris"
+	var age int = 30
+	var height float64= 6.1
+
+	fmt.Println("There was a man called " + name  + ".")
+	fmt.Println("He was",age,  "years old.")
+	fmt.Println("He was" , height , "foot tall.")
+}
