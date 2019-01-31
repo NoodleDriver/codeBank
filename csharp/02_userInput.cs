98841f257a28541cdcd271c3a272d13991713da6
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 19:14:02 +0000

    Update 02_userInput.cs

--- csharp/02_userInput.cs
+++ csharp/02_userInput.cs
@@ -4,12 +4,11 @@ using System;
 
 class Hello{
     public static void Main(){
-
+	    
         string name;
 
 	Console.WriteLine("What is your name?\n");
-    name = Console.ReadLine();
-    Console.WriteLine("Hello, {0}. Have a great day.", name );
-
+    	name = Console.ReadLine();
+    	Console.WriteLine("Hello, {0}. Have a great day.", name );
     }
 }
