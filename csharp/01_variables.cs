fce76cc137d08402ed2341f6db082e1ab08c30a5
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 16:00:02 +0000

    Create 01_variables.cs

---
+++ csharp/01_variables.cs
@@ -0,0 +1,20 @@
+//Compile by mcs Hello.cs
+
+using System;
+
+class Hello{
+    public static void Main(){
+
+        string name = "Boris";
+        int age = 30;
+        double height = 6.1;
+        char grade = 'A';
+
+
+
+	Console.WriteLine("There was a man called {0}.", name);
+    Console.WriteLine("He was {0} years old.", age);
+    Console.Write("He was {0} foot tall and ", height);
+    Console.WriteLine("Scored an {0} on the test.", grade);
+    }
+}
