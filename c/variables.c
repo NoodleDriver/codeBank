8828ce2b90bfc8b29e7059c180867f72f75f59d3
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 15:55:44 +0000

    Create variables.c

---
+++ c/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    char name[100] = "Boris";
+    int age = 30;
+    float height = 6.1;
+
+   printf("There was a man called %s.\n", name);
+   printf("He was %i years old.\n", age);
+   printf("He was %.1f foot tall.\n", height);
+   return 0;
+}
