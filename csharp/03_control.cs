7492d200a00f52557db5d70fd5d05aca2b20216a
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 16:45:17 +0000

    Create 03_control.cs

---
+++ csharp/03_control.cs
@@ -0,0 +1,48 @@
+//Compile by mcs Hello.cs
+
+using System;
+
+class Hello{
+    public static void Main(){
+
+        int select;
+
+	Console.WriteLine("select a number");
+    select = Convert.ToInt32(Console.ReadLine());
+
+   if(select == 0){
+      Console.WriteLine("If. Select = 0");
+   }
+   else if(select < 2){
+       Console.WriteLine("else If. Select < 2");
+   }
+   else{
+       Console.WriteLine("else");
+   }
+
+   switch(select){
+
+       case 0:
+        Console.WriteLine("switch. Select = 0");
+       break;
+
+       case 1:
+        Console.WriteLine("switch. Select = 1");
+       break;
+
+       default:
+        Console.WriteLine("switch. Select = default");
+       break;
+   }
+
+   while(select > 0){
+       Console.WriteLine("while select > 0; select = {0}", select);
+       select--;
+   }
+
+   for(int i = 0; i < 5; i++){
+       Console.WriteLine("for. i = {0}", i);
+   }
+
+    }
+}
