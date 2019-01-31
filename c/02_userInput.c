febe7523ed4662843d7fbe65aa2baf1e62143641
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 19:10:51 +0000

    Update 02_userInput.c

--- c/02_userInput.c
+++ c/02_userInput.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main(){
-
     char name[100];
 
-   printf("What is your name?\n");
-   scanf("%s", &name);
+    printf("What is your name?\n");
+    scanf("%s", &name);
+    printf("Hello, %s. Have a great day.\n", name);
 
-   printf("Hello, %s. Have a great day.\n", name);
-
-   return 0;
+    return 0;
 }
