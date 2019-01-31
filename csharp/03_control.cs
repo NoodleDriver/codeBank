a3b843f092ed69baeee63849d62fb35b47f356ce
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 19:15:10 +0000

    Update 03_control.cs

--- csharp/03_control.cs
+++ csharp/03_control.cs
@@ -8,41 +8,40 @@ class Hello{
         int select;
 
 	Console.WriteLine("select a number");
-    select = Convert.ToInt32(Console.ReadLine());
+    	select = Convert.ToInt32(Console.ReadLine());
 
-   if(select == 0){
-      Console.WriteLine("If. Select = 0");
-   }
-   else if(select < 2){
-       Console.WriteLine("else If. Select < 2");
-   }
-   else{
-       Console.WriteLine("else");
-   }
+   	if(select == 0){
+      		Console.WriteLine("If. Select = 0");
+   	}
+   	else if(select < 2){
+       		Console.WriteLine("else If. Select < 2");
+   	}
+   	else{
+       		Console.WriteLine("else");
+   	}
 
-   switch(select){
+   	switch(select){
 
-       case 0:
-        Console.WriteLine("switch. Select = 0");
-       break;
+       		case 0:
+        		Console.WriteLine("switch. Select = 0");
+       		break;
 
-       case 1:
-        Console.WriteLine("switch. Select = 1");
-       break;
+       		case 1:
+        		Console.WriteLine("switch. Select = 1");
+       		break;
 
-       default:
-        Console.WriteLine("switch. Select = default");
-       break;
-   }
+      		 default:
+        		Console.WriteLine("switch. Select = default");
+       		break;
+   	}
 
-   while(select > 0){
-       Console.WriteLine("while select > 0; select = {0}", select);
-       select--;
-   }
+   	while(select > 0){
+       		Console.WriteLine("while select > 0; select = {0}", select);
+       		select--;
+   	}
 
-   for(int i = 0; i < 5; i++){
-       Console.WriteLine("for. i = {0}", i);
-   }
-
+   	for(int i = 0; i < 5; i++){
+       		Console.WriteLine("for. i = {0}", i);
+  	 }
     }
 }
