222e0a84348161cd9f0a56e5411d5baaf42df0bc
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 19:12:14 +0000

    Update 02_userInput.cpp

--- cpp/02_userInput.cpp
+++ cpp/02_userInput.cpp
@@ -2,11 +2,11 @@
 #include <string>
 
 int main(){
-
     std::string name;
 
     std::cout << "What is your name?\n";
     std::cin >> name;
     std::cout << "Hello, " << name << ". Have a great day.";
+    
     return 0;
 }
