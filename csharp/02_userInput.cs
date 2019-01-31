29df722bccbafc160d15bd7149f872a682c69b0c
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 16:01:03 +0000

    Create 02_userInput.cs

---
+++ csharp/02_userInput.cs
@@ -0,0 +1,15 @@
+//Compile by mcs Hello.cs
+
+using System;
+
+class Hello{
+    public static void Main(){
+
+        string name;
+
+	Console.WriteLine("What is your name?\n");
+    name = Console.ReadLine();
+    Console.WriteLine("Hello, {0}. Have a great day.", name );
+
+    }
+}
