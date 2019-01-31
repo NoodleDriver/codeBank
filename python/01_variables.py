32dc1ba84c72ac477b94267cec98a49049f9988a
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 16:03:08 +0000

    Create 01_variables.py

---
+++ python/01_variables.py
@@ -0,0 +1,7 @@
+name = "Boris"
+age = 30
+height = 6.1
+
+print ("There was a man called {}.".format(name))
+print ("He was {} years old.".format(age))
+print ("He was {} foot tall.".format(height))
