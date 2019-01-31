f6d92df0dfdb3b0447bef54cdddfc7e1ab61fcfb
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-01-31 21:17:02 +0000

    Create 03_control.py

---
+++ python/03_control.py
@@ -0,0 +1,18 @@
+selector = int(input("select a number"))
+
+print(selector)
+ 
+if selector == 0:
+    print("if. Selector = 0")
+elif selector == 1:
+   print ("else if. Selector = 1")
+else:
+   print ("else")
+
+while selector > 0:
+   print("while I = {}".format(selector))
+   selector = selector - 1
+
+nums = [1,2,3,4]
+for i in nums:
+   print(i)
